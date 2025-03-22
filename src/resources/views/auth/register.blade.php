41c732c0ea79bcb2dcaea5114cbd1ac9fc012ac5
kameda <kamedashiori@icloud.com>
2025-03-23 07:04:05 +0900

    register update

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -56,9 +56,17 @@
                 </div>
             </div>
         </div>
-        <div class="form__button">
-            <button class="form__button-submit" type="submit">登録</button>
-        </div>
+        <div class="form__group">
+            <div class="form__group-title">
+                <span class="form__label--item">確認用パスワード</span>
+            </div>
+            <div class="form__group-content">
+                <div class="form__input--text">
+                    <input type="password" name="password_confirmation" placeholder="例: coachtech1106" />
+                </div>
+                <div class="form__button">
+                    <button class="form__button-submit" type="submit">登録</button>
+                </div>
     </form>
 </div>
 @endsection
